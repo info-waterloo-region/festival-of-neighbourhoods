79d1d9a61ac8f0e97b50d59a5400597ce627c528
Todd Turnbull <shingaling@gmail.com>
2017-01-23 12:14:54 -0500

    Javascript files for 2016 activities

---
+++ data/unregistered-2016.js
@@ -0,0 +1,25 @@
+---
+title: Festival of Neighbourhoods Unregistered Activities
+---
+var unregistered2015 = {
+  "type": "FeatureCollection", 
+  "features": [{% for act in site.data.unregistered-activities-2016 %}{% assign place = site.data.codes | where:"postalCode",act.code %}{
+    "type": "Feature", 
+    "geometry": {
+      "type": "Point", 
+      "coordinates": [
+        {{place[0].lon}},
+        {{place[0].lat}}
+      ]
+    },
+    "properties": {
+      "neighbourhood": "{{act.neighbourhood}}",
+      "activity": {
+        "date": "{{act.date | date: "%b %d, %Y"}}",
+        "activity": "{{act.activity | escape}}",
+        "quote": ""
+      }
+    }
+  }{% unless forloop.last %},{% endunless %}{% endfor %}]
+}
+
